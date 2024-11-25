3f4e1d6ddc5750c04c3cb8b1cafaa65fbec491d7
admin <komly@yandex.ru>
2024-11-25 05:38:05 +0300

    fix readme

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="demucs_acapella",
-    version="1.0.0",
+    version="1.0.1",
     description="A CLI tool for extracting acapellas using Demucs and managing audio metadata.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="komly",
     author_email="komly@yandex.ru",
     url="https://github.com/komly/demucs_acapella",
